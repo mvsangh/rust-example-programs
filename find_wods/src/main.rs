830393e39765e810232a54688397abc30f25b786
mvsangh <msangh.devloper@gmail.com>
2024-12-19 12:51:54 +0530

    added find pattern from file in find_words project

--- find_wods/src/main.rs
+++ find_wods/src/main.rs
@@ -1,23 +1,50 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+use std::thread::sleep;
+use std::time::Duration;
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    FindInSentence { sentence: String, word: String },
+    FindInFile { path: PathBuf, pattern: String },
+}
+
 fn main() {
-    println!("Enter the <sentence>,<word-to-find>");
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::FindInSentence { sentence, word } => {
+            find_words_in_sentence(&sentence, &word);
+        }
+        Commands::FindInFile { path, pattern } => {
+            sleep(Duration::from_secs(2));
+            find_pattern_from_file(path, pattern);
+        }
+    }
+}
 
-    let mut args = String::new();
-    std::io::stdin().read_line(&mut args).unwrap();
+fn find_words_in_sentence(sentence: &str, word: &str) {
+    println!("Sentence: {:?}", sentence);
+    println!("Word: {:?}", word);
 
-    let args = args.trim();
-    println!("args {:?}", args);
-
-    let mut parts = args.split(",");
-    let sentence = parts.next().unwrap();
-    let word = parts.next().unwrap();
-
-    println!("sentence {:?}", sentence);
-    println!("word {:?}", word);
     if sentence.contains(word) {
-        println!("Word is present in the sentence");
+        println!("Word is present in the sentence\n");
+    } else {
+        println!("Word is not present in the sentence\n");
     }
-    else {
-        println!("Word is not present in the sentence");
-    }
+}
 
+fn find_pattern_from_file(filename: PathBuf, pattern: String) {
+    let file_content = std::fs::read_to_string(filename).unwrap();
+
+    for line in file_content.lines() {
+        if line.contains(&pattern) {
+            println!("{}", line);
+        }
+    }
 }
